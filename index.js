b09efbfd65c80c4580a897e5c080cd74e02f98b6
cesckrodriguez <104809109+cesckrodriguez@users.noreply.github.com>
2022-08-09 22:06:19 -0500

    primera clase node nano npm y rm

---
+++ index.js
@@ -0,0 +1,14 @@
+	console.log("Estoy escribiendo codigo desde la terminal y ademas ya corregi la ortografia");
+var colors = require("colors")
+
+function sumar(x,y){
+	let suma = x+y
+	console.log("la suma es: ".red + suma)
+}
+
+sumar(30,40);
+
+
+for(let i=0; i<5; i++){
+	console.log("francisco ".blue , i )
+}
